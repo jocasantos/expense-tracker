20f49eb052ce1eb97daa6955ca1292425d71e780
giga <tacuara10@gmail.com>
2024-02-11 12:34:47 +0000

    add the main text on hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Dashboard() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Controla as tuas <span className="text-blue-600">despesas</span> de
+            maneira simples e rápida.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Registre-se agora e descubra como um controle eficiente pode ser o
+            caminho para uma poupança sólida e uma vida financeira mais
+            realizadora
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/entrar" className={buttonVariants()}>
+              Entrar
+            </Link>
+            <Link href="/registar">
+              <Button variant="ghost">Regista-te &rarr;</Button>
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </>
+  );
 }
