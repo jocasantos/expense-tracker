872367d5142c83385ce8bb9730e105434e06b07c
giga <tacuara10@gmail.com>
2024-02-11 17:37:01 +0000

    add logo hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Dashboard() {
   return (
     <>
       <MaxWidthWrapper>
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <div className="h-60 w-60 relative -mb-10">
+            <Image src="/logo.jpg" alt="logo" fill />
+          </div>
+          <h1 className="z-20 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Controla as tuas <span className="text-blue-600">despesas</span> de
             maneira simples e rápida.
           </h1>
@@ -25,7 +29,18 @@ export default function Dashboard() {
             </Link>
           </div>
         </div>
+        {/* <div className="flex justify-center">
+          <Image
+            src="/mock-expense-tracker.png"
+            alt="App image example"
+            width={1000}
+            height={1000}
+            style={{ width: "100%", height: "auto", maxWidth: "768px" }}
+          />
+        </div> */}
       </MaxWidthWrapper>
+
+      {/* Images of the app large and small screen (2) */}
     </>
   );
 }
